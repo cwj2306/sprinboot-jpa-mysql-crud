95ee6d8b499f564fa9d9fc45d2ac82c3762ffa43
woojeong <cwj2306@gmail.com>
2019-11-29 16:47:08 +0900

    시큐리티 구현

--- src/main/java/com/cos/crud/model/Board.java
+++ src/main/java/com/cos/crud/model/Board.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToOne;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/cos/crud/model/User.java
+++ src/main/java/com/cos/crud/model/User.java
@@ -11,7 +11,6 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
